cf231102556179c5de5e9fd4feeaa1c7df5e6380
ha <afa_shi@163.com>
2018-08-14 17:13:47 +0800

    Initial commit

---
+++ eureka-client/src/main/java/com/afa/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.afa.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zhou
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
